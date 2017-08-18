e93b1dc666e5a69de64486b13dbb3a09d758f295
yehia67 <yehiatarek67@gmail.com>
2017-08-18 04:16:26 +0200

    add plus button in the action bar to add new task

---
+++ app/src/main/java/com/example/yehia/todo_list/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.yehia.todo_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.AB_add) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
