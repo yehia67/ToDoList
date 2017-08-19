c7711e1e31a70b5cb8046f5c05049989cd8053b0
yehia67 <yehiatarek67@gmail.com>
2017-08-19 21:44:27 +0200

    try to add one task temporally and see if the pop window will work or not

--- app/src/main/java/com/example/yehia/todo_list/MainActivity.java
+++ app/src/main/java/com/example/yehia/todo_list/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yehia.todo_list;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,22 +13,36 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 ListView listView;
+ArrayList<String> tasks = new ArrayList<>();
  String taskname = getIntent().getStringExtra("name") ;
+    String recive = getIntent().getStringExtra("key");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.lv);
+        tasks.add(taskname);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,tasks);
+        listView.setAdapter(arrayAdapter);
          listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
              @Override
              public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                  int itemPosition     = position;
                  String  itemValue    = (String) listView.getItemAtPosition(position);
-                 Toast.makeText(getApplicationContext(),
-                         "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-                         .show();
+                 AlertDialog.Builder builder = new AlertDialog.Builder(null);
+                 builder.setTitle("task " + itemValue);
+                 builder.setMessage(recive);
+
+                 // add a button
+                 builder.setPositiveButton("OK", null);
+
+                 // create and show the alert dialog
+                 AlertDialog dialog = builder.create();
+                 dialog.show();
              }
          });
 
