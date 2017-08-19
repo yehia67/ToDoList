99b8f7a7afffe52695973df1636b6369058adf3b
yehia67 <yehiatarek67@gmail.com>
2017-08-19 02:43:03 +0200

    need to know how to acces th items of list view.

--- app/src/main/java/com/example/yehia/todo_list/AddTask.java
+++ app/src/main/java/com/example/yehia/todo_list/AddTask.java
@@ -1,13 +1,36 @@
 package com.example.yehia.todo_list;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddTask extends AppCompatActivity {
-
+   EditText TaskName,TaskDescripition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        TaskName = (EditText) findViewById(R.id.ET_taskname);
+        TaskDescripition = (EditText) findViewById(R.id.ET_taskdescr);
     }
+public void Onclick(View view)
+{
+    switch (view.getId())
+    {
+        case R.id.bt_discard:
+            startActivity(new Intent(this,AddTask.class));
+            break;
+        case R.id.bt_save:
+            Intent intent = new Intent(this,AddTask.class);
+            String getName = TaskName.getText().toString();
+            String getDesc = TaskDescripition.getText().toString();
+            String send = "task name :- " + getName + "\n" + "Description :" + "\n"+ getDesc;
+            intent.putExtra("key",send);
+            intent.putExtra("name",getName);
+            startActivity(intent);
+    }
+}
+
 }
--- app/src/main/java/com/example/yehia/todo_list/MainActivity.java
+++ app/src/main/java/com/example/yehia/todo_list/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.yehia.todo_list;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView listView;
+ String taskname = getIntent().getStringExtra("name") ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.lv);
+         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                 int itemPosition     = position;
+                 String  itemValue    = (String) listView.getItemAtPosition(position);
+                 Toast.makeText(getApplicationContext(),
+                         "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                         .show();
+             }
+         });
+
     }
 
     @Override
