de18f3c87c669917aec6fbb04fc1de240aa7e7b3
yehia67 <yehiatarek67@gmail.com>
2017-08-21 12:19:36 +0200

    create class adapter and still have bugs

--- app/src/main/java/com/example/yehia/todo_list/MainActivity.java
+++ app/src/main/java/com/example/yehia/todo_list/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.RecycleView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
